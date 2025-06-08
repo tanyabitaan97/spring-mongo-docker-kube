7b6a657cac39622a9cf2975f0c7399aa7ebe4095
communications@headstreams.org <tanya.chaudhary1@tcs.com>
2025-06-08 10:44:33 +0530

    first commit

---
+++ src/main/java/mongodb/UserRepository.java
@@ -0,0 +1,10 @@
+package mongodb;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
