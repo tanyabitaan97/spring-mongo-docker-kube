74c7a386119fe5d6c1b5115d0afc3f6e61027710
Tanya Chaudhary <49437326+tanyabitaan97@users.noreply.github.com>
2025-07-04 09:41:19 +0530

    Update and rename UserApplication.java to EventsApplication.java

--- src/main/java/mongodb/EventsApplication.java
+++ src/main/java/mongodb/EventsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserApplication {
+public class EventsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        SpringApplication.run(EventsApplication.class, args);
     }
 }
