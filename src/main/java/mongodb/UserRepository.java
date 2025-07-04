48b1913893fa92097630e6737024204ed69083eb
Tanya Chaudhary <49437326+tanyabitaan97@users.noreply.github.com>
2025-07-04 09:40:05 +0530

    Update UserRepository.java

--- src/main/java/mongodb/UserRepository.java
+++ src/main/java/mongodb/UserRepository.java
@@ -1,10 +1,14 @@
 package mongodb;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<Events, String> {
+
+	List<Events> findByNameOfEvent(String name);
 
 }
