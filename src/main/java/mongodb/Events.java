ebf85abec62a9f019bee097246b86d174bb3b3bc
Tanya Chaudhary <49437326+tanyabitaan97@users.noreply.github.com>
2025-07-04 09:40:37 +0530

    Update and rename User.java to Events.java

---
+++ src/main/java/mongodb/Events.java
@@ -0,0 +1,81 @@
+package mongodb;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "userevents")
+public class Events {
+	
+	    @Id
+	    private String id;
+	    
+	    private String typeOfInteractionData;
+	    
+	    private String dateOfInteraction;
+	    
+	    private String nameOfEvent;
+	    
+	    private String createdAt;
+	    
+	    private double amount;
+	    
+	    private String dataUpdatedBy;
+	    
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getTypeOfInteractionData() {
+			return typeOfInteractionData;
+		}
+
+		public void setTypeOfInteractionData(String typeOfInteractionData) {
+			this.typeOfInteractionData = typeOfInteractionData;
+		}
+
+		public String getDateOfInteraction() {
+			return dateOfInteraction;
+		}
+
+		public void setDateOfInteraction(String dateOfInteraction) {
+			this.dateOfInteraction = dateOfInteraction;
+		}
+
+		public String getNameOfEvent() {
+			return nameOfEvent;
+		}
+
+		public void setNameOfEvent(String nameOfEvent) {
+			this.nameOfEvent = nameOfEvent;
+		}
+
+		public String getCreatedAt() {
+			return createdAt;
+		}
+
+		public void setCreatedAt(String createdAt) {
+			this.createdAt = createdAt;
+		}
+
+		public double getAmount() {
+			return amount;
+		}
+
+		public void setAmount(double amount) {
+			this.amount = amount;
+		}
+
+		public String getDataUpdatedBy() {
+			return dataUpdatedBy;
+		}
+
+		public void setDataUpdatedBy(String dataUpdatedBy) {
+			this.dataUpdatedBy = dataUpdatedBy;
+		}
+	    
+}
